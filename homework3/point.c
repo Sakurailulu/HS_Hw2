cd4d92c10fe4d1b30754dce4670209586d273a4d
FelicToken <47868673+FelicToken@users.noreply.github.com>
2019-11-18 11:24:51 -0500

    Update point.c

--- homework3/point.c
+++ homework3/point.c
@@ -15,62 +15,65 @@
 
 #define BUFFER_SIZE 1024
 
-struct PointOnGraph{
-	char[BUFFER_SIZE] ID;
+struct Point{
+	char* ID;
 	float XPos;
 	float YPos;
 	bool isBaseStation;
 };
+/**
+*initialized the point and set memory
+*/
+struct Point* initial_point(){
+	struct Point* point=(Point*)malloc(sizeof(struct Point));
+	point->ID=malloc(BUFFER_SIZE*sizeof(char));
+	point->XPos=-1;
+	point->YPos=-1;
+	point->isBaseStation=false;
+}
+/**
+*convert string of reachable to an array of Point
+*
+*/
+int loadNode(char* message,struct Point** points){
+//REACHABLE [NumReachable] [ReachableList]([ID] [XPosition] [YPosition]) 
+    char word[BUFFER_SIZE];
+    int count=0;
+    int temp=0;
+    int NumReachable=0;
+    if(message[strlen(message)-1]!='\n'){
+        strcat(message,"\n");
+    }
+    int curr=0;
+    for(int i=0;i<strlen(message);i++){
+        if( message[i]==' '|| message[i]=='\n'){
+            if(count==0){
+                count++;
+                continue;
+            }
+            else if(count==1){
+                NumReachable=atoi(word);
+                points=(struct Points**)malloc(NumReachable*sizeof(struct Point*));
+            }
+            else{
+                        points[curr] = initial_point();
+                        points[curr]
+                        curr++;
+             
+            }
+            count++;
+            memset(word,0, sizeof(char*));
+            temp=0;
+           
+        }
+        else{
+            word[temp]=message[i];
+            temp++;
+        }
 
 
-// void print_points(struct PointOnGraph* all_the_points, int total_number_of_points){
-// 	for(int i=0; i<total_number_of_points; i++){
-// 		printf("At location %d  %d   is  a ", all_the_points[i]->XPos,all_the_points[i]->YPos);
-// 		if(all_the_points[i]->isBaseStation == true){
-// 			printf("basestation\n" );
-// 		}else{
-// 			printf("censor\n");
-// 		}
-// 	}
-// }
-
-// void initialize_point(struct BaseStation* Station, struct Client* Client){
-// 	if(Station == NULL){
-// 		struct PointOnGraph new_point;
-// 		memset(Client->ID,0, sizeof(Client->ID));
-// 		new_point->ID = Client->ID;
-// 		new_point->XPos = Client->XPos;
-// 		new_point->YPos = Client->YPos;
-// 		isBaseStation = false;
-// 	}else if(Client == NULL){
-// 		struct PointOnGraph new_point;
-// 		memset(Station->ID,0, sizeof(Station->ID));
-// 		new_point->ID = Station->ID;
-// 		new_point->XPos = Station->XPos;
-// 		new_point->YPos = Station->YPos;
-// 		isBaseStation = true;
-
-// 	}else{
-// 		bool tag = false;
-// 		//neither is null, idk problem i guess;
-// 	}
-// }
-// void set_point_ID(char* _id){
-// 	ID = _id;
-// }
-// void set_point_X(float _x){
-// 	XPos = _x;
-// }
-// void set_point_Y(float _y){
-// 	YPos = _y;
-// }
-// //this is to set if the struct passed in is a base station ,= true
-// //if the struct is not a base station, it's a sensor ,= false
-// void set_is_base_staiton(bool _setbool){
-// 	isBaseStation = _setbool;
-// }
-
-//struct PointOnGraph* all_points(struct BaseStation* Station,)
+    }
+}
 
 
 
